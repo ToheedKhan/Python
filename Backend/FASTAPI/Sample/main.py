16f890eb66d9e4d35106ce1f3f5700093e4c160c
Toheed Khan <consultant-toheed.khan@mcb.mu>
2025-05-18 01:52:41 +0530

    Path parameter names

--- Backend/FASTAPI/Sample/main.py
+++ Backend/FASTAPI/Sample/main.py
@@ -1,5 +1,20 @@
+#
+# from fastapi import FastAPI
+# app = FastAPI()
+# @app.get("/")
+# async def index():
+#    return {"message": "Hello World :-)"}
+#
+
+import uvicorn
 from fastapi import FastAPI
 app = FastAPI()
 @app.get("/")
 async def index():
-   return {"message": "Hello World :-)"}
+   return {"message": "Hello World"}
+@app.get("/hello/{name}")
+async def hello(name):
+   return {"name": name}
+@app.get("/hello/{name}/{age}")
+async def hello(name,age):
+   return {"name": name, "age":age}
