9c38db0468f73a007aaccd737c9b1d3221c1a57a
Toheed Khan <consultant-toheed.khan@mcb.mu>
2025-05-18 02:04:57 +0530

    Path Parameter with type and validation conditions

--- Backend/FASTAPI/Sample/main.py
+++ Backend/FASTAPI/Sample/main.py
@@ -18,3 +18,17 @@ async def hello(name):
 @app.get("/hello/{name}/{age}")
 async def hello(name,age):
    return {"name": name, "age":age}
+
+# If the URL given in the browsers address bar is http://localhost:8000/hello/Ravi/20, The data of Ravi and 20 will be assigned to variables name and age respectively. The browser displays the following JSON response −
+
+# {"name":"Ravi","age":"20"}
+
+# Path Parameters with Types
+@app.get("/hello/{name}/{age}")
+async def hello(name:str,age:int):
+   return {"name": name, "age":age} 
+
+# The parameter 'name' is a path parameter and 'age' is a query parameter
+@app.get("/hello/{name}")
+async def hello(name:str,age:int):
+   return {"name": name, "age":age}
